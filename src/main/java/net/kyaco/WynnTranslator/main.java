d8ac89a51d7d47389b14f19c03c6477a0a1c589e
kyaco <kyacof@gmail.com>
2019-10-14 20:38:22 +0900

    First commit!

---
+++ src/main/java/net/kyaco/WynnTranslator/main.java
@@ -0,0 +1,12 @@
+package net.kyaco.WynnTranslator;
+import net.fabricmc.api.ModInitializer;
+
+public class main implements ModInitializer{
+
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello fabric world!");
+
+	}
+	
+}
