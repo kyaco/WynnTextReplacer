9acb855141f50a24b1f99497a5f42c4d3999cc24
kyaco <kyacof@gmail.com>
2019-10-31 07:42:50 +0900

    イベントの割り当て作成途中

--- src/main/java/net/kyaco/wynntr/ENUSTranslationStorage.java
+++ src/main/java/net/kyaco/wynntr/ENUSTranslationStorage.java
@@ -1,8 +1,13 @@
 package net.kyaco.wynntr;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,8 +15,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.resource.language.TranslationStorage;
 import net.minecraft.network.MessageType;
 import net.minecraft.resource.ResourceManager;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -40,29 +44,29 @@ public class ENUSTranslationStorage extends TranslationStorage {
 		}
 		System.out.println("[WynnTextReplacer] Reloaded text maps.");
 	}
-
 	public Text ReverseTranslate(Text text, MessageType location) {
 		if (!"Wynncraft".equals(MinecraftClient.getInstance().getCurrentServerEntry().name))
 			return text;
 
 		TranslatableText translatableText;
 		translatableText = tryNormalReplace(text);
-		if (translatableText == null) translatableText = tryRegexpReplace(text);
-		if (translatableText == null) {
-			recordUnregisterdText(text, location);
-			return text;
+		if (translatableText == null)
+		{
+			translatableText = tryRegexpReplace(text);
+			if (translatableText == null)
+			{
+				recordUnregisterdText(text, location);
+				return text;
+			}
 		}
 		return tryToFixEvents(text, translatableText);
 	}
-
-
 	private TranslatableText tryNormalReplace(Text text) {
 		String formattedString = text.asFormattedString();
 		String text_id = rawReversDict.get(formattedString);
 		if (text_id != null) return new TranslatableText(text_id);
 		return null;
 	}
-
 	private TranslatableText tryRegexpReplace(Text text) {
 		String formattedString = text.asFormattedString();
 		for (Map.Entry<Pattern, String> entry : this.regxpReversDict.entrySet()) {
@@ -77,32 +81,48 @@ public class ENUSTranslationStorage extends TranslationStorage {
 		}
 		return null;
 	}
+	private Text tryToFixEvents(Text text, TranslatableText translatableText) {
+		List<Text> clickEventTexts = GetTextWithStyle(text, (s)->{return s.getClickEvent() != null;});
+		List<Text> hoverEventTexts = GetTextWithStyle(text, (s)->{return s.getHoverEvent() != null;});
+		List<Text> insertionTexts = GetTextWithStyle(text, (s)->{return s.getInsertion() != null;});
+		if (clickEventTexts.size() == 0 && hoverEventTexts.size() == 0 && insertionTexts.size() == 0) return translatableText;
 
+		String str[] = translatableText.asFormattedString().split("§r");
+		List<Text> splitText = new ArrayList<Text>();
+
+		System.out.println("----------------------");
+		for(String s: str) {
+			Text t = new LiteralText(s);
+			splitText.add(new LiteralText(s));
+			System.out.println("str: " + s);
+		}
+		System.out.println("----");
+		System.out.println(text);
+		// for (Text t: clickEventTexts) {
+		// 	System.out.println(t.getStyle().getClickEvent());
+		// }
+		System.out.println("----------------------");
+
+		
+		return translatableText;
+	}
 	private void recordUnregisterdText(Text text, MessageType location) {
 		System.out.println("[wynntr:unregisterd text of " + location + "] \"" + text.asFormattedString() + "\"");
 		System.out.println("[wynntr:unicode escape version]" + convertToUnicode(text.asFormattedString()));
 	}
-
-	private Text tryToFixEvents(Text text, TranslatableText translatableText) {
-		String str = translatableText.asFormattedString();
-
-		for(Text subtext: text.getSiblings()) {
-			Style style = subtext.getStyle();
-
-			ClickEvent clickEvent = style.getClickEvent();
-			HoverEvent hoverEvent = style.getHoverEvent();
-			String insertion =style.getInsertion();
-			if (clickEvent != null) {
-				// todo
-			}
-			if (hoverEvent != null) {
-				// todo
-			}
-			if (insertion != null) {
-				// todo
-			}
+	private List<Text> GetTextWithStyle(Text text, Function<Style, Boolean> function) {
+		List<Text> resultTexts = new ArrayList<Text>();
+		Queue<Text> q = new LinkedList<Text>();
+		q.add(text);
+		while(!q.isEmpty()) {
+			Text head = q.poll();
+			for(Text child: head.getSiblings()) q.add(child);
+			
+			Style style = head.getStyle();
+			if (style == null) continue;
+			if (function.apply(style)) resultTexts.add(head);
 		}
-		return translatableText;
+		return resultTexts;
 	}
 
 	private static String convertToUnicode(String original)
